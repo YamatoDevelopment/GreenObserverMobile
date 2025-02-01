1233b256da3aab8303d51f5b6933296742720890
Cu64 <dongduongdev@gmail.com>
2025-02-01 17:50:53 -0500

    feat: WIP. persist settings to sharedpref

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -8,40 +10,41 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  SharedPreferences? prefs;
+
   @override
   void initState() {
     super.initState();
+    _loadSharedPreferences();
+  }
+
+  Future<void> _loadSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Form(
-              child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 150),
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Display Name',
-                        ),
+          child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 150),
+              child: FormBuilder(
+                  key: _formKey,
+                  child: Column(children: <Widget>[
+                    FormBuilderTextField(
+                      name: 'displayName',
+                      decoration: InputDecoration(
+                        labelText: 'Display Name',
                       ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Username',
-                        ),
+                    ),
+                    FormBuilderTextField(
+                      name: 'username',
+                      decoration: InputDecoration(
+                        labelText: 'Username',
                       ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: ElevatedButton(
-                                onPressed: () {}, child: const Text('Save'))),
-                      )
-                    ],
-                  )))),
+                    ),
+                  ])))),
     );
   }
 }
