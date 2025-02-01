da9e927d2a41c8e6a51314d46e52813325e40856
Ctrl_c_n1nja <ffilippov179@gmail.com>
2025-02-01 14:53:14 -0500

    camera page, issues with scaling and angle

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,121 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:geolocator/geolocator.dart';
+  import 'package:flutter_map/flutter_map.dart';
+  import 'package:latlong2/latlong.dart';
+  import 'package:geolocator/geolocator.dart';
+  import 'camera.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  class HomePage extends StatefulWidget {
+    const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final mapController = MapController();
-  int _selectedIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _getUserLocation(); // Fetch location on startup
+    @override
+    State<HomePage> createState() => _HomePageState();
   }
 
-  Future<void> _getUserLocation() async {
-    try {
-      Position position = await _determinePosition();
-      LatLng currentLocation = LatLng(position.latitude, position.longitude);
-      mapController.move(currentLocation, 15.5); // Move map to user's location
-    } catch (e) {
-      print("Error getting location: $e");
-    }
-  }
+  class _HomePageState extends State<HomePage> {
+    final mapController = MapController();
+    int _selectedIndex = 0;
 
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
+    @override
+    void initState() {
+      super.initState();
+      _getUserLocation(); // Fetch location on startup
     }
 
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied.');
+    Future<void> _getUserLocation() async {
+      try {
+        Position position = await _determinePosition();
+        LatLng currentLocation = LatLng(position.latitude, position.longitude);
+        mapController.move(currentLocation, 15.5); // Move map to user's location
+      } catch (e) {
+        print("Error getting location: $e");
       }
     }
 
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error('Location permissions are permanently denied.');
+    Future<Position> _determinePosition() async {
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
+      }
+
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied.');
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error('Location permissions are permanently denied.');
+      }
+
+      return await Geolocator.getCurrentPosition();
+    }
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+      if (index == 1) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CameraPage()),
+        );
+      }
+    }
+    // Screens for bottom navigation
+    static const List<Widget> _widgetOptions = <Widget>[
+      Text('Home Page'),
+      Text('Business Page'),
+      Text('School Page'),
+    ];
+
+    Widget _buildIcon(IconData icon, int index) {
+      bool isSelected = _selectedIndex == index;
+      return Container(
+        decoration: isSelected
+            ? BoxDecoration(
+          shape: BoxShape.circle,
+          color: Color(0xFF18453B).withOpacity(0.2), // Light MSU Green background
+        )
+            : null,
+        padding: isSelected ? EdgeInsets.all(8) : EdgeInsets.zero,
+        // Circle padding
+        child: Icon(icon, size: isSelected ? 32 : 28,
+            color: isSelected ? Color(0xFF18453B) : Colors.grey),
+      );
+    }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+
+        body: FlutterMap(
+          mapController: mapController,
+          options: MapOptions(
+            initialCenter: LatLng(55.7509167, 037.6170556),
+            initialZoom: 15.5,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.greenobserver',
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.map, 0),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.add, 1),
+              label: 'Add',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.settings, 2),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xFF18453B), // MSU Green
+          onTap: _onItemTapped,
+          backgroundColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+
+      );
     }
 
-    return await Geolocator.getCurrentPosition();
   }
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-  // Screens for bottom navigation
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('Business Page'),
-    Text('School Page'),
-  ];
-
-  Widget _buildIcon(IconData icon, int index) {
-    bool isSelected = _selectedIndex == index;
-    return Container(
-      decoration: isSelected
-          ? BoxDecoration(
-        shape: BoxShape.circle,
-        color: Color(0xFF18453B).withOpacity(0.2), // Light MSU Green background
-      )
-          : null,
-      padding: isSelected ? EdgeInsets.all(8) : EdgeInsets.zero,
-      // Circle padding
-      child: Icon(icon, size: isSelected ? 32 : 28,
-          color: isSelected ? Color(0xFF18453B) : Colors.grey),
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FlutterMap(
-        mapController: mapController,
-        options: MapOptions(
-          initialCenter: LatLng(55.7509167, 037.6170556),
-          initialZoom: 15.5,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.greenobserver',
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: _buildIcon(Icons.map, 0),
-            label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: _buildIcon(Icons.add, 1),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: _buildIcon(Icons.settings, 2),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xFF18453B), // MSU Green
-        onTap: _onItemTapped,
-        backgroundColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-      ),
-
-    );
-  }
-
-}
---
+++ lib/camera.dart
@@ -0,0 +1,145 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as img;
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController _controller;
+  late List<CameraDescription> _cameras;
+  late CameraDescription _camera;
+  bool _isCameraInitialized = false;
+  bool _isFrontCamera = false;
+  String _imagePath = ''; // Initialize as an empty string
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    _cameras = await availableCameras();
+    _camera = _cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.back);
+
+    _controller = CameraController(
+      _camera,
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+
+    await _controller.initialize();
+
+    // Set the camera to preview in the correct orientation
+    setState(() {
+      _isCameraInitialized = true;
+    });
+  }
+
+  Future<void> _toggleCamera() async {
+    setState(() {
+      _isFrontCamera = !_isFrontCamera;
+      _camera = _isFrontCamera
+          ? _cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.front)
+          : _cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.back);
+    });
+
+    await _controller.dispose();
+    _controller = CameraController(_camera, ResolutionPreset.high);
+    await _controller.initialize();
+
+    setState(() {});
+  }
+
+  Future<void> _takePicture() async {
+    try {
+      final directory = await getTemporaryDirectory();
+      final path = '${directory.path}/${DateTime.now().millisecondsSinceEpoch}.png';
+
+      await _controller.takePicture().then((XFile file) async {
+        // Load the image using the image package to fix the orientation
+        final bytes = await file.readAsBytes();
+        img.Image? image = img.decodeImage(Uint8List.fromList(bytes));
+
+        if (image != null) {
+          // Rotate the image if needed
+          img.Image fixedImage = img.copyRotate(image, 90); // Rotate by 90 degrees or -90 depending on your device
+
+          // Save the rotated image back to the file
+          final newPath = '${directory.path}/${DateTime.now().millisecondsSinceEpoch}_fixed.png';
+          final newFile = File(newPath)..writeAsBytesSync(img.encodePng(fixedImage));
+
+          setState(() {
+            _imagePath = newFile.path; // Set the new image path
+          });
+        }
+      });
+    } catch (e) {
+      print("Error capturing picture: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Camera Page')),
+      body: _isCameraInitialized
+          ? Stack(
+        children: [
+          // CameraPreview with a 90-degree rotation applied
+          Transform.rotate(
+            angle: 90 * 3.1415927 / 180,  // Convert 90 degrees to radians
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: CameraPreview(_controller),
+            ),
+          ),
+          Positioned(
+            top: 50,
+            left: 10,
+            child: IconButton(
+              icon: Icon(Icons.switch_camera, color: Colors.white),
+              onPressed: _toggleCamera,
+            ),
+          ),
+          Positioned(
+            bottom: 80,
+            left: MediaQuery.of(context).size.width / 2 - 30,
+            child: IconButton(
+              icon: Icon(Icons.camera, color: Colors.white, size: 60),
+              onPressed: _takePicture,
+            ),
+          ),
+          if (_imagePath.isNotEmpty)
+            Positioned(
+              bottom: 20,
+              left: MediaQuery.of(context).size.width / 2 - 30,
+              child: GestureDetector(
+                child: CircleAvatar(
+                  backgroundImage: FileImage(File(_imagePath)),
+                  radius: 30,
+                ),
+              ),
+            ),
+        ],
+      )
+          : Center(child: CircularProgressIndicator()),
+    );
+  }
+}
